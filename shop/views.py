762455329f76f194aa8b836dda20c5932f6e0987
Magnus Dilling <magnusdilling@gmail.com>
2020-10-26 13:50:13 +0100

    loads of small changes

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
-def product_list(request, category_slug=None):
+def home(request):
+    return render(request, 'shop/home.html')
+
+def product_list(request, category_slug=False):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
         return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+        #return render(request, 'shop/home.html')
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, '/shop/product/detail.html', {'product': product})
+
+
+
